b3c476616fe57322b5e0b8c8155e28aafdbeddde
legut <levcenkokola06@gmail.com>
2025-10-18 22:35:34 +0300

    3

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -49,7 +49,7 @@ export default function HomePage() {
     try {
       await api.post(
         "/api/user/update-clicks",
-        { progress: localProgress + 0.001 },
+         {progress: 0.001 },
         { headers: { Authorization: `Bearer ${localStorage.getItem("authToken")}` } }
       );
     } catch (err) {
